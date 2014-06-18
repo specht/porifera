133dec61f43dfdda38cf5ee69a0dbe19af33fc63
nham <hamann.nick@gmail.com>
2014-06-18 10:08:32 -0400

    Tweak comments and formatting in boot.asm

--- boot.asm
+++ boot.asm
@@ -1,19 +1,19 @@
 %macro SEG_ASM 1 ; Type
-        ; base = 0x0, limit = 0xfffff
-        dw 0xffff ; lowest 16 bits of limit
-        dw 0x0 ; lowest 16 bits of base
-        db 0x0 ; bits 16-23 of base
-        db 10010000b | (%1); first byte of flags. 
-        ; bit 4: Descriptor type. 1 = code or data
-        ; bits 5-6: privilege level: 0 for ring 0, the highest
-        ; bits 7: segment present, 1 = yes
+    ; base = 0x0, limit = 0xfffff
+    dw 0xffff ; lowest 16 bits of limit
+    dw 0x0 ; lowest 16 bits of base
+    db 0x0 ; bits 16-23 of base
+    db 10010000b | (%1); first byte of flags. 
+    ; bit 4: Descriptor type. 1 = code or data
+    ; bits 5-6: privilege level: 0 for ring 0, the highest
+    ; bits 7: segment present, 1 = yes
 
-        db 11001111b ; 1111 = bits 16-19 of limit, rest are flags
-        ; bit 4: "Available for use by system software". not sure
-        ; bit 5: 64-bit code segment: I guess we set this if want to switch to long mode
-        ; bit 6: Default operation size. 1 = 32-bit segment
-        ; bit 7: granularity, 1 = multiplies limit by 4K = 0x1000, so segment spans 4 GB of memory
-        db 0x0 ; bits 24-31 of base
+    db 11001111b ; 1111 = bits 16-19 of limit, rest are flags
+    ; bit 4: "Available for use by system software". not sure
+    ; bit 5: 64-bit code segment: I guess we set this if want to switch to long mode
+    ; bit 6: Default operation size. 1 = 32-bit segment
+    ; bit 7: granularity, 1 = multiplies limit by 4K = 0x1000, so segment spans 4 GB of memory
+    db 0x0 ; bits 24-31 of base
 %endmacro
 
 
@@ -43,36 +43,34 @@ start:
     or al, 1
     mov cr0, eax
 
-    jmp    (SEG_KCODE<<3):start32 ; something something long jump to flush the pipeline. don't actually understand the segment here. TODO: figure out
+    jmp    (SEG_KCODE<<3):start32 
     ; segment descriptors are 8 bytes and the code entry is the second entry
-    ; so that's why it's 8. but does this load something in the CS segment? what
-    ; exactly is it doing?
+    ; so that's why it's 8. this is also the only way to populate the CS register
+    ; From xv6 notes: "Enabling protected mode does not immediately change how the processor translates logical to physical addresses; it is only when one loads a new value into a segment register that the processor reads the GDT and changes its internal segmentation settings. One cannot directly modify %cs, so instead the code executes an ljmp (far jump) instruction, which allows a code segment selector to be specified."
 
 start32:
 use32
-  ; now we are in protected mode
+    ; now we are in protected mode
 
-  ; TODO: this has something to do with the data segment entry in GDT; clarify.
-  ; http://www.cse.iitd.ernet.in/~sbansal/os/lec/l13.html has this to say:
-  ; "The first few instructions in 32-bit mode (8458-8461) setup DS, ES, and SS registers to point to the second segment descriptor in privileged mode (SEG_KDATA * 8)."
-  mov    eax, SEG_KDATA<<3
-  mov    ds, eax
-  mov    es, eax
-  mov    ss, eax
-  mov    fs, eax
-  mov    gs, eax
+    ; populate remaining segment registers with the data segment
+    mov    eax, SEG_KDATA<<3
+    mov    ds, eax
+    mov    es, eax
+    mov    ss, eax
+    mov    fs, eax
+    mov    gs, eax
 
 
-; Something about clearing the screen
-  mov edi, 0xb8000
-  mov ecx, 80*25*2
-  mov al, 0
-  rep stosb
+    ; Something about clearing the screen
+    mov edi, 0xb8000
+    mov ecx, 80*25*2
+    mov al, 0
+    rep stosb
 
 
-; TODO: figure out what call does
-  mov    esp, start
-  call   bootmain
+    ; TODO: figure out how the call works. how is the bootmain symbol visible to assembly?
+    mov    esp, start
+    call   bootmain
 
 
 gdt:
